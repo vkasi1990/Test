67115590d0ee338a49ae69a983fbf4cc0313950e
Kasiviswanathan V <vkasi1990@gmail.com>
2018-10-22 23:45:59 +0530

    Update SimpleExamples.java

---
+++ SimpleExamples.java
@@ -0,0 +1,13 @@
+package com.examples.SimpleExamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class SimpleExamples 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello" );
+    }
+}
